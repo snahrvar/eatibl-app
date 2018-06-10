dfbdd39fc33bf0610d52e63623532748a739676a
Matt <matt@oasiscode.com>
2018-06-10 13:29:39 -0400

    Reworked homepage emailing.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../_services/user.service';
 import { FunctionsService } from '../_services/functions.service';
 import { Router } from '@angular/router';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpClientModule } from '@angular/common/http'
 import * as decode from 'jwt-decode';
 
 
@@ -11,8 +13,22 @@ import * as decode from 'jwt-decode';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  email1: any;
+  email2: any;
+  email3: any;
+  name: any;
+  message: any;
+  apiUrl = environment.apiURL;
+  formOne = false;
+  formTwo = false;
+  formThree = false;
 
-  constructor(public userService: UserService, private router: Router, private functions: FunctionsService) { }
+  constructor(
+    public userService: UserService,
+    private router: Router,
+    private functions: FunctionsService,
+    private http: HttpClient,
+  ) { }
 
   ngOnInit() {
   }
@@ -35,6 +51,38 @@ export class HomeComponent implements OnInit {
       var restaurantId = user.restaurant_fid;
       this.router.navigate(['/restaurant/' + restaurantId + '/bookings']);
     }
+  }
+
+  emailFormSubmit(type){
+    var postObject: any;
+    if(type == 'User')
+      postObject = {
+        email: this.email1,
+        type: type
+      }
+    if(type == 'Restaurant')
+      postObject = {
+        email: this.email2,
+        type: type
+      }
+    if(type == 'General')
+      postObject = {
+        name: this.name,
+        email: this.email3,
+        message: this.message,
+        type: type
+      }
+    this.http.post(this.apiUrl + '/homepage/emailForm', postObject)
+      .subscribe(
+        res => {
+          if(res)
+            if(type == 'User')
+              this.formOne = true;
+            if(type == 'Restaurant')
+              this.formTwo = true;
+            if(type == 'General')
+              this.formThree = true;
+        });
   }
 
 }
